c7e3d484c592dc63ca17d50965492cb4140c6101
ZbrushVFX <aka.device@gmail.com>
2024-01-27 12:17:33 +0300

    update: change inxdec and aobut files

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return 'Hello, Flask!'
+  return render_template('index.html')
+  #return redirect(url_for('about'))
+
+@app.route('/about')
+def about():
+  return render_template('about.html')
 
 if __name__ == '__main__':
   app.run(debug=True)
