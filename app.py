ea5cf61819196ecb638b30fa8d884c8a26207ecf
ZbrushVFX <aka.device@gmail.com>
2024-01-29 23:54:19 +0300

    update: app.py db sql

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 Bootstrap(app)
 
 # DB configuration
-db = yaml.load(open('db.yaml'))
+db = yaml.full_load(open('db.yaml'))
 app.config['MYSQL_HOST'] = db['mysql_host']
 app.config['MYSQL_USER'] = db['mysql_user']
 app.config['MYSQL_PASSWORD'] = db['mysql_password']
@@ -17,6 +17,13 @@ mysql = MySQL(app)
 
 @app.route('/')
 def index():
+  cursor = mysql.connection.cursor()
+  #cursor.execute("INSERT INTO user VALUES(%s)", (['John']))
+  #mysql.connection.commit()
+  result_value = cursor.execute("SELECT * FROM user")
+  if result_value > 0 :
+    users = cursor.fetchall()
+    print(users)
   return render_template('index.html')
   #return redirect(url_for('about'))
 
